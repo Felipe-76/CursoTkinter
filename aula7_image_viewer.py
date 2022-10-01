44835e371b026aef9ef566c88f7aa193c8b66e55
Felipe-76 <felipe.leitao14@gmail.com>
2022-09-30 21:28:07 -0300

    Menu de status

--- aula7_image_viewer.py
+++ aula7_image_viewer.py
@@ -25,12 +25,29 @@ def colocar_imagem(pag):
     global imagem
     imagem.grid_forget()
     imagem = Label(image = imgs[pag%5], width=400, height=500)
-    imagem.grid(column=0, row=0, columnspan=4)
+    imagem.grid(column=0, row=0, columnspan=3)
     # Mudar numero da pagina
     label_contador.grid_forget()
-    label_contador = Label(text=f"{pag%5+1}")
-    label_contador.grid(column=1, row=1)
+    label_contador = Label(text=f"pagina {pag%5+1} de {len(imgs)}", bd=1,borderwidth=1 , relief=SUNKEN, anchor=E)
+    label_contador.grid(column=0, row=2, columnspan=3, sticky=W+E)
+    checar_botoes(pag)
 
+def checar_botoes(pag):
+    global button_direita
+    global button_esquerda
+    if(pag == 0):
+        button_esquerda = Button(text="<", padx=5, pady=5, command=passar_esquerda, state=DISABLED)
+        button_direita = Button(text=">", padx=5, pady=5, command=passar_direita)
+    elif(pag == len(imgs)-1):
+        button_esquerda = Button(text="<", padx=5, pady=5, command=passar_esquerda)
+        button_direita = Button(text=">", padx=5, pady=5, command=passar_direita, state=DISABLED)        
+    else:
+        button_esquerda = Button(text="<", padx=5, pady=5, command=passar_esquerda)
+        button_direita = Button(text=">", padx=5, pady=5, command=passar_direita)
+    button_esquerda.grid_forget()
+    button_direita.grid_forget()
+    button_esquerda.grid(column=0, row=1)
+    button_direita.grid(column=2, row=1, pady=5)
 
 
 # Passando as imagens
@@ -42,20 +59,20 @@ img_5 = ImageTk.PhotoImage(Image.open("imgs_7/im5.jpeg"))
 imgs = [img_1, img_2, img_3, img_4, img_5]
 imagem = Label(image = imgs[pagina%5], width=400, height=500)
 
-# Colocando as imagens
-imagem.grid(column=0, row=0, columnspan=4)
-
 # Criando os botões e o contador
-button_esquerda = Button(text="<", padx=5, pady=5, command=passar_esquerda)
-label_contador = Label(text=f"{pagina+1}")
+button_esquerda = Button(text="<", padx=5, pady=5, command=passar_esquerda, state=DISABLED)
+label_contador = Label(text=f"pagina {pagina%5+1} de {len(imgs)}", bd=1,borderwidth=1 , relief=SUNKEN, anchor=E)
 button_fechar = Button(text="Close app", padx=70,pady=5, command=root.destroy)
 button_direita = Button(text=">", padx=5, pady=5, command=passar_direita)
 
+# Colocando as imagens
+imagem.grid(column=0, row=0, columnspan=3)
+
 # Colocando o Menu
 button_esquerda.grid(column=0, row=1)
-label_contador.grid(column=1, row=1)
-button_fechar.grid(column=2, row=1)
-button_direita.grid(column=3, row=1)
+button_fechar.grid(column=1, row=1)
+button_direita.grid(column=2, row=1, pady=5)
 
+label_contador.grid(column=0, row=2, columnspan=3, sticky=W+E)
 
 root.mainloop()
